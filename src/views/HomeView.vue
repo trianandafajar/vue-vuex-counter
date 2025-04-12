26d3c2988e6885b5f4c5c4ebf95ead554a37d85b
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-12 11:12:46 +0700

    Update View Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- Menampilkan komponen Counter -->
     <Counter />
+    
+    <!-- Menampilkan komponen CounterSquare -->
     <CounterSquare />
+    
+    <!-- Menampilkan komponen Button untuk mengubah counter -->
     <Button />
+    
+    <!-- Menampilkan komponen InputColorCode untuk mengubah warna -->
     <InputColorCode />
   </div>
 </template>
@@ -26,13 +33,21 @@ div {
   margin-bottom: 10px;
 }
 
+/* Gaya untuk komponen counter */
 .counter {
   font-size: 80px;
 }
 
+/* Gaya untuk tombol */
 .buttons button {
   font-size: 40px;
   width: 100px;
   margin: 0 10px;
+}
+
+/* Styling untuk gambar logo */
+img {
+  display: block;
+  margin: 0 auto 20px;
 }
 </style>
