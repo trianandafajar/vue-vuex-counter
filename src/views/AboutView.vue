27f3d9ac251d4c84a225edc50469aaf914912278
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-12 11:12:35 +0700

    Update View About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an page about</h1>
+    <h1>This is a page about</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "AboutView",
+};
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  font-size: 36px;
+  color: #333;
+}
+</style>
